fcb94811cd10a89b2a30c7d9eab0930f7bcfd641
smh136783 <smh136783@alibaba-inc.com>
2018-10-10 09:36:59 +0800

    s

--- mobx/config-overrides.js
+++
@@ -1,7 +0,0 @@
-const rewireMobX = require('react-app-rewire-mobx');
-
-module.exports = function override(config, env) {
-  config = rewireMobX(config, env);
-  console.log('config', config);
-  return config;
-}
